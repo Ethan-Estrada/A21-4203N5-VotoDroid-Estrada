a8717daf675a0de3896df1821a7f6758ac8692f5
Ethan Estrada <2056092@cegepmonpetit.ca>
2021-11-04 23:53:10 -0400

    FCT :  Ajout du écran de résultats
    Ajout de la mise en page et du graphique

--- Tp2/VoteDroid/app/src/main/java/org/estrada/votedroid/Resultat.java
+++ Tp2/VoteDroid/app/src/main/java/org/estrada/votedroid/Resultat.java
@@ -6,11 +6,19 @@ import android.view.View;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
 import org.estrada.votedroid.databinding.ActivityResultatBinding;
+
+import java.util.ArrayList;
 
 public class Resultat extends AppCompatActivity {
 
     private ActivityResultatBinding binding;
+    public ArrayList<BarEntry> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,6 +27,29 @@ public class Resultat extends AppCompatActivity {
         binding = ActivityResultatBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        setTitle("Résultats");
+
+        binding.txtviewMoyenne.setText("Moyenne : "+Integer.sum(2,2)       );
+        binding.txtviewEcart.setText("Ecart : "+Integer.sum(3,3)       );
+
+
+        BarChart barChart = findViewById(R.id.barChart);
+
+        notes.add(new BarEntry(3,2));
+        notes.add(new BarEntry(4,1));
+        notes.add(new BarEntry(5,4));
+
+        BarDataSet barDataSet = new BarDataSet(notes,"Notes");
+
+
+        BarData barData = new BarData(barDataSet);
+
+        barChart.getXChartMin();
+        barChart.setFilterTouchesWhenObscured(true);
+        barChart.setData(barData);
+        barChart.getDescription().setText("Description Label");
+        barChart.animateY(2000);
+
 
     }
 }
