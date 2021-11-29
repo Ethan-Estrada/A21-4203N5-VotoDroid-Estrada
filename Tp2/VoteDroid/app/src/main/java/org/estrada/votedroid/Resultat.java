b0535cf5d62c372fa1e8bb3c84b24d74251c365e
Ethan Estrada <2056092@cegepmonpetit.ca>
2021-11-29 13:07:56 -0500

    COR : Correction du barChart
    Graphique fidèle à l'exemple demandé

--- Tp2/VoteDroid/app/src/main/java/org/estrada/votedroid/Resultat.java
+++ Tp2/VoteDroid/app/src/main/java/org/estrada/votedroid/Resultat.java
@@ -5,11 +5,19 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.DefaultAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 
 import org.estrada.votedroid.databinding.ActivityResultatBinding;
 
@@ -29,8 +37,8 @@ public class Resultat extends AppCompatActivity {
         setContentView(view);
         setTitle("Résultats");
 
-        binding.txtviewMoyenne.setText("Moyenne : "+Integer.sum(2,2)       );
-        binding.txtviewEcart.setText("Ecart : "+Integer.sum(3,3)       );
+        binding.txtviewMoyenne.setText("Moyenne : ");
+        binding.txtviewEcart.setText("Ecart : ");
 
 
         BarChart barChart = findViewById(R.id.barChart);
@@ -40,17 +48,34 @@ public class Resultat extends AppCompatActivity {
         notes.add(new BarEntry(5,4));
 
         BarDataSet barDataSet = new BarDataSet(notes,"Notes");
-
+        barDataSet.setDrawValues(false);
 
         BarData barData = new BarData(barDataSet);
 
-        barChart.ma
-        barChart.getXChartMin();
-        barChart.setFilterTouchesWhenObscured(true);
         barChart.setData(barData);
         barChart.getDescription().setText("Description Label");
         barChart.animateY(2000);
 
+        barChart.setMaxVisibleValueCount(6);
+        barChart.setPinchZoom(false);
+        barChart.setDrawGridBackground(false);
 
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setAxisMinimum(0);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setGranularity(1); // only intervals of 1 day
+        xAxis.setLabelCount(5);
+        xAxis.setValueFormatter(new DefaultAxisValueFormatter(0));
+
+        YAxis leftAxis = barChart.getAxisLeft();
+        leftAxis.setLabelCount(8, false);
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        leftAxis.setSpaceTop(15f);
+        leftAxis.setGranularity(1);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setValueFormatter(new DefaultAxisValueFormatter(0));
+        barChart.getDescription().setEnabled(false);
+        barChart.getAxisRight().setEnabled(false);
     }
 }
