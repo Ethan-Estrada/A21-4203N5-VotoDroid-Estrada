650ebad6f87c326ba107932858ee3cbcc4951e03
Ethan Estrada <2056092@cegepmonpetit.ca>
2021-12-16 11:21:01 -0500

    FCT : Affiche de la liste des questions
    Ajout du code renvoyant la liste des questions

--- Tp2/VoteDroid/app/src/main/java/org/estrada/votedroid/bd/MonDao.java
+++ Tp2/VoteDroid/app/src/main/java/org/estrada/votedroid/bd/MonDao.java
@@ -2,13 +2,25 @@ package org.estrada.votedroid.bd;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.Query;
 
 import org.estrada.votedroid.modele.VDQuestion;
+import org.estrada.votedroid.modele.VDVote;
+
+import java.util.List;
 
 @Dao
-public interface MonDao {
+public abstract class MonDao {
+
     @Insert
-    Long insertQuestion(VDQuestion v);
+    public abstract Long insertQuestion(VDQuestion v);
+
+    @Insert
+    public abstract Long creerVote(VDVote v);
+
+@Query("select * from VDQuestion")
+public abstract List<VDQuestion> tousLesQuestions();
+
 
 //TODO Compléter les autres actions
 
